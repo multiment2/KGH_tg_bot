11ce2333d833b3d160222b1802e92a011a61d4d2
multiment <multiment@gmail.com>
2022-08-02 21:50:55 +0300

    Add token key

--- config_run.py
+++ config_run.py
@@ -1,4 +1,9 @@
-TOKEN = ""
+from server_for_all.config_flask import PYTHONDONTWRITEBYTECODE
+
+
+TOKEN = '5454971848:AAFqPyrY6SLDONle1jJ3Xw9zJlGy_jKma9Q'
+name = 'ДС "Ростки"'
 bot_user_name = "KGH_bot"
 
 DEBUG = True
+PYTHONDONTWRITEBYTECODE = 1
