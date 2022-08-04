d3e6e353a81dfe441cb8d51fe5fe15f1dc4fcab1
multiment <multiment@gmail.com>
2022-08-04 21:36:12 +0300

    Update

--- config_run.py
+++ config_run.py
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-from server_for_all.config_flask import PYTHONDONTWRITEBYTECODE
-
-
 TOKEN = '5454971848:AAFqPyrY6SLDONle1jJ3Xw9zJlGy_jKma9Q'
+
 name = 'ДС "Ростки"'
+
 bot_user_name = "KGH_bot"
-=======
-#TOKEN = ""
-#bot_user_name = "KGH_bot"
->>>>>>> 800371c5a3803313823e7a9730313891b45e034c
 
 DEBUG = True
-PYTHONDONTWRITEBYTECODE = 1
+
