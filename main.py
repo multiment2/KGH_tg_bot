060408ffef5cc43274de604bedb1c0c2f93f6f67
Михаил Сорокин <multiment@gmail.com>
2022-08-05 10:25:49 +0000

    Скорректировал главный файл бота

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
-import config_run
+from config_run import *
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Dispatcher
 
 app = Flask(__name__)
 app.config.from_pyfile("config_run.py")
@@ -11,25 +12,29 @@ updater = Updater(token=TOKEN, use_context=True)
 
 dispatcher = updater.dispatcher
 
-start_handler = CommendHandler('start', start)  #Объединяем функцию и обработчик
-dispatcher.add(start_handler)  #Добавляем обработчик в диспетчер
-
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-# говорим обработчику `MessageHandler`, если увидишь текстовое 
-# сообщение (фильтр `Filters.text`)  и это будет не команда 
-# (фильтр ~Filters.command), то вызови функцию `echo()`
-dispatcher.add(echo_handler) #теперь бот будет слушать все сообщения чата
-
-updater.pulling()  #Слушай сервера Telegram
-
 def start(update, context):
   context.bot.send_message(chat_id=update.effective_chat.id, text="LALALALa") 
   # `bot.send_message` это метод Telegram API
     # `update.effective_chat.id` - определяем `id` чата, 
     # откуда прилетело сообщение 
 
+start_handler = CommandHandler('start', start)  #Объединяем функцию и обработчик
+dispatcher.add_handler(start_handler)  #Добавляем обработчик в диспетчер
+
 def echo(update, context):
   pass
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+# говорим обработчику `MessageHandler`, если увидишь текстовое 
+# сообщение (фильтр `Filters.text`)  и это будет не команда 
+# (фильтр ~Filters.command), то вызови функцию `echo()`
+dispatcher.add_handler(echo_handler) #теперь бот будет слушать все сообщения чата
+
+updater.start_polling()  #Слушай сервера Telegram
+
+
+
+
 
 @app.route ("/")
 def hello_func():
