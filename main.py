67d59936690a6d7b90e712b94af436dce8045359
multiment2 <51969777+multiment2@users.noreply.github.com>
2022-08-09 13:46:11 +0000

    Stable verson

--- main.py
+++ main.py
@@ -10,17 +10,18 @@ app.config.from_pyfile("config_run.py")
 #bot = telegram.Bot(token = TOKEN)
 updater = Updater(token=TOKEN, use_context=True)
 
+
 dispatcher = updater.dispatcher
 
 def start(update, context):
-  context.bot.send_message(chat_id=context.bot.effective_chat.id, text="Я запустился")
+	context.bot.send_message(chat_id=update.effective_chat.id, text="Я запустился")
 
 start_handler = CommandHandler('start', start)  #Объединяем функцию и обработчик
 dispatcher.add_handler(start_handler)  #Добавляем обработчик в диспетчер
 
 #echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-# говорим обработчику `MessageHandler`, если увидишь текстовое 
-# сообщение (фильтр `Filters.text`)  и это будет не команда 
+# говорим обработчику `MessageHandler`, если увидишь текстовое
+# сообщение (фильтр `Filters.text`)  и это будет не команда
 # (фильтр ~Filters.command), то вызови функцию `echo()`
 #dispatcher.add_handler(echo_handler) #теперь бот будет слушать все сообщения чата
 
@@ -32,8 +33,8 @@ updater.start_polling()  #Слушай сервера Telegram
 
 #@app.route ("/")
 def hello_func():
-  return "Hello gay"
+	return "Hello gay"
 
 
 if __name__ == "__main__":
-  app.run()
+	app.run()
