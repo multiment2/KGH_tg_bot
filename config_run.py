4b3c9749c0f0b2536d01f09e936cb86fb795887d
multiment2 <51969777+multiment2@users.noreply.github.com>
2022-08-12 10:03:24 +0000

    Update viber_bot file

--- config_run.py
+++ config_run.py
@@ -1,12 +1,10 @@
 TOKEN = '5454971848:AAFqPyrY6SLDONle1jJ3Xw9zJlGy_jKma9Q'
 
-VIBER_TOKEN = '4fa311bc7a67dc3f-62d39ee041a8b35c-19012c2b7bca367a'
+VIBER_TOKEN = '4fa45c5514a7e67a-25a9ef07fb3d9258-9c0008fa29bb9e31'
 
 name = 'ДС "Ростки"'
 
 bot_user_name = "KGH_bot"
-
-viber_bot_user_name = "kgh_tg_bot"
 
 group_chat_id = 0
 
--- main_viber.py
+++ main_viber.py
@@ -5,14 +5,14 @@ from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
 from viberbot.api.messages.text_message import TextMessage
 
-from viberbot.api.viber_requests import ViberConversationRequest
+from viberbot.api.viber_requests import ViberConversationStartedRequest
 from viberbot.api.viber_requests import ViberFailedRequest
 from viberbot.api.viber_requests import ViberMessageRequest
 from viberbot.api.viber_requests import ViberSubscribedRequest
 from viberbot.api.viber_requests import ViberUnsubscribedRequest
 
-app = Flask(__main__)
-viber = Api(BotConfiguration(auth_token=VIBER_TOKEN, name=viber_bot_user_name))
+app = Flask(__name__)
+viber = Api(BotConfiguration(auth_token=VIBER_TOKEN, name=bot_user_name, avatar='371524012.jpg'))
 
 @app.route("/", methods = ['POST'])
 def incoming():
@@ -24,4 +24,13 @@ def incoming():
     if isinstance(viber_request, ViberMessageRequest):
         message = viber_request.message
         viber.send_messages(viber_request.sender.id, [message])
+
+    elif isinstance(viber_request, ViberSubscribedRequest):
+        viber.send_messages(viber_request.get_user.id, [TextMessage(text = "Спасибо")])
+
+    return Response(status=200)
+
+if __name__ == "__main__":
+    app.run()
+
 
